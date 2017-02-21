72e2aa3b8c9f2e8f730a66553ee30232e0d474b4
Kendra Tate <kendratate@comcast.net>
2017-02-21 10:19:17 -0700

    Added Exercise folder

--- src/pages/exercises/exercises.ts
+++ src/pages/exercises/exercises.ts
@@ -3,18 +3,23 @@
  */import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ExerciseService } from '../../providers/exercise-service';
+import {ExerciseDetailModal} from "../exerciseDetail/exerciseDetail";
 
 @Component({
   selector: 'page-exerciseItem',
   templateUrl: 'exercises.html'
 })
 
-
 export class ExerciseItemPage {
 
   constructor(private nav: NavController, private exerciseService: ExerciseService){
   }
-
   exercise = this.exerciseService.exercise;
 
+  openDetail(id: number){
+    this.navCtrl.push(ExerciseDetailModal, {
+      id : id
+    });
+   }
 }
+

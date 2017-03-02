0e57643e5035f06c89defe080c4e23f8e2d6cd09
Kendra Tate <kendratate@comcast.net>
2017-03-02 12:15:00 -0700

    Fixed Checkbox error

--- src/pages/day-detail/day-detail.ts
+++ src/pages/day-detail/day-detail.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {UserDataService} from "../../providers/user-data";
 
@@ -7,7 +7,7 @@ import {UserDataService} from "../../providers/user-data";
   templateUrl: 'day-detail.html'
 })
 export class DayDetailPage {
-  dayIndex: number;
+dayIndex: number;
   userWorkout: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, private userDataService: UserDataService) {
     this.userWorkout = userDataService.userWorkout;
@@ -17,24 +17,20 @@ export class DayDetailPage {
     this.dayIndex = this.navParams.data;
   }
 
-  customTrackBy(index: number, obj: any): any {
-    return index;
-  }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad DayDetailPage');
   }
 
   allWorkoutsComplete(): void{
-    let allWorkouts = this.userWorkout[this.index].workouts.length;
+    let allWorkouts = this.userWorkout[this.dayIndex].workouts.length;
     let finishedWorkOut = 0;
     for (let c = 0; c < allWorkouts; c++){
-      if (this.userWorkout[this.index].workouts.completed === true){
+      if (this.userWorkout[this.dayIndex].workouts.completed === true){
         finishedWorkOut ++;
       }
     }
     if (allWorkouts === finishedWorkOut){
-        this.userWorkout[this.index].workoutCompleted = true;
+        this.userWorkout[this.dayIndex].workoutCompleted = true;
     }
 
 
